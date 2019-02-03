f7583454fa0fc48051f4b8c7d511a7e9d1d34d71
Ty Conner <tyconner@itcproductions.com>
2019-02-03 12:43:54 -0500

    Update 2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql

--- Database/2-BaseUpdates/2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql
+++ Database/2-BaseUpdates/2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql
@@ -1,7 +1,18 @@
 USE `ace_world`;
 
 ALTER TABLE `landblock_instance_link` 
+;
+ALTER TABLE `landblock_instance_link` RENAME INDEX `parent_child_guuidx` TO `parent_child_uidx`;
+ALTER TABLE `landblock_instance_link` ALTER INDEX `parent_child_uidx` INVISIBLE;
+
+ALTER TABLE `landblock_instance_link` 
+ADD UNIQUE INDEX `child_uidx` (`child_GUID` ASC) VISIBLE;
+;
+
+/*
+ALTER TABLE `landblock_instance_link` 
 DROP FOREIGN KEY `instance_link`;
 ALTER TABLE `landblock_instance_link` 
-DROP INDEX `parent_child_guuidx` ;
+DROP INDEX `parent_child_uidx` ;
 ;
+*/
