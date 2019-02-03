5afd122ab2ec21037cb5f21af63978b01b771a16
Ty Conner <tyconner@itcproductions.com>
2019-02-03 00:36:51 -0500

    Update 2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql

--- Database/2-BaseUpdates/2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql
+++ Database/2-BaseUpdates/2019-02-02-02-Drop_FK_On_Landblock_Instance_Links.sql
@@ -1,6 +1,6 @@
 USE `ace_world`;
 
-ALTER TABLE landblock_instance_link` 
+ALTER TABLE `landblock_instance_link` 
 DROP FOREIGN KEY `instance_link`;
 ALTER TABLE `landblock_instance_link` 
 DROP INDEX `parent_child_guuidx` ;
