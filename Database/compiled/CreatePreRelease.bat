837be282ebed5218ec304f54fc6871bac13475e5
Ty Conner <tyconner@itcproductions.com>
2019-03-07 00:14:29 -0500

    Change the way triggers are added to World Database
    This sets up the export to use the correct definer user for triggers

--- Database/compiled/CreatePreRelease.bat
+++ Database/compiled/CreatePreRelease.bat
@@ -1,3 +1,8 @@
 set /p password=<password.txt
 del 16PY-db.sql
-"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt > 16PY-db.sql
+"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --skip-triggers > 16PY-db.sql
+@echo off
+echo. >> 16PY-db.sql
+copy /b 16PY-db.sql + ..\1-Base\WorldTriggers.sql 16PY-db.sql
+echo. >> 16PY-db.sql
+@echo on
--- Database/compiled/CreateWorldBase.bat
+++ Database/compiled/CreateWorldBase.bat
@@ -1,3 +1,7 @@
+@echo off
 set /p password=<password.txt
 del ..\1-Base\WorldBase.sql
-"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt > ..\1-Base\WorldBase.sql
+"C:\Program Files\MySql\MySQL Server 8.0\bin\mysqldump.exe" --user=root --password=%password% --databases ace_world --add-drop-database --add-drop-table --create-options --quote-names --lock-tables --dump-date --flush-privileges --set-gtid-purged=AUTO --disable-keys --tz-utc --add-locks --extended-insert --opt --skip-triggers > ..\1-Base\WorldBase.sql
+echo. >> ..\1-Base\WorldBase.sql
+copy /b ..\1-Base\WorldBase.sql + ..\1-Base\WorldTriggers.sql ..\1-Base\WorldBase.sql
+echo. >> ..\1-Base\WorldBase.sql
