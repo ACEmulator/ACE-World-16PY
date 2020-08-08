6c78e38482bbe9733befc8a850d0759b7e8a9573
Zurgash <i.am.zurgash@gmail.com>
2020-08-08 15:52:09 -0400

    Update 27655 Renegade Chest.sql
    Fixed Salted Meats quest to drop 100 meats per wiki info.
    https://asheron.fandom.com/wiki/Renegade_Bone_Charm_Quest

--- Database/3-Core/9 WeenieDefaults/SQL/Chest/Container/27655 Renegade Chest.sql
+++ Database/3-Core/9 WeenieDefaults/SQL/Chest/Container/27655 Renegade Chest.sql
@@ -54,4 +54,4 @@ VALUES (27655,   1,   33558692) /* Setup */
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (27655, -1, 27682, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Renegade Bone Charm (27682) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
      , (27655, -1, 27707, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Salted Meat (27707) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */
-     , (27655, -1, 27669, 0, 1, 1, 2, 8, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Tumerok Salted Meat (27669) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
+     , (27655, -1, 27669, 0, 1, 1, 2, 8, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Tumerok Salted Meat (27669) (x1 up to max of 1) - Regenerate upon PickUp - Location to (re)Generate: Contain */;
