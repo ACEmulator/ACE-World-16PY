85bd3962b3e73e6bd047ef8ea6f97dc41484d5ae
Ty Conner <tyconner@itcproductions.com>
2022-06-22 14:27:54 -0400

    Restore missing Recipe Mod data (#76)
    * Updated InventoryTreasureType Export
    
    * Updated RecipeMod export

--- Database/3-Core/4 CraftTable/SQL/00019 Mana Potion.sql
+++ Database/3-Core/4 CraftTable/SQL/00019 Mana Potion.sql
@@ -3,6 +3,9 @@ DELETE FROM `recipe` WHERE `id` = 19;
 INSERT INTO `recipe` (`id`, `unknown_1`, `skill`, `difficulty`, `salvage_Type`, `success_W_C_I_D`, `success_Amount`, `success_Message`, `fail_W_C_I_D`, `fail_Amount`, `fail_Message`, `success_Destroy_Source_Chance`, `success_Destroy_Source_Amount`, `success_Destroy_Source_Message`, `success_Destroy_Target_Chance`, `success_Destroy_Target_Amount`, `success_Destroy_Target_Message`, `fail_Destroy_Source_Chance`, `fail_Destroy_Source_Amount`, `fail_Destroy_Source_Message`, `fail_Destroy_Target_Chance`, `fail_Destroy_Target_Amount`, `fail_Destroy_Target_Message`, `data_Id`, `last_Modified`)
 VALUES (19, 0, 38 /* Alchemy */, 180, 0, 379 /* Mana Potion */, 1, 'You brew a mana potion.', 0, 0, 'You fail to brew a mana potion.', 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, 0, '2005-02-09 10:00:00');
 
+INSERT INTO `recipe_mod` (`recipe_Id`, `executes_On_Success`, `health`, `stamina`, `mana`, `unknown_7`, `data_Id`, `unknown_9`, `instance_Id`)
+VALUES (19, True, 0, 0, -25, False, 0, 0, 0);
+
 DELETE FROM `cook_book` WHERE `recipe_Id` = 19;
 
 INSERT INTO `cook_book` (`recipe_Id`, `source_W_C_I_D`, `target_W_C_I_D`, `last_Modified`)
